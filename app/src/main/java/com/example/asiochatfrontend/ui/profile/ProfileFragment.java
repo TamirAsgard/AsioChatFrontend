35833bd8e62a6ec6466d10e8c78961bd6c4fa622
TamirAsgard <Tamir.s@asgardsys.co.il>
2025-04-09 09:02:29 +0300

    ui commits

--- app/src/main/java/com/example/asiochatfrontend/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/asiochatfrontend/ui/profile/ProfileFragment.java
@@ -1,4 +1,23 @@
 package com.example.asiochatfrontend.ui.profile;
 
-public class ProfileFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.asiochatfrontend.R;
+
+public class ProfileFragment extends Fragment {
+    private ProfileViewModel viewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        viewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        return view;
+    }
 }
--- app/src/main/java/com/example/asiochatfrontend/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/asiochatfrontend/ui/profile/ProfileViewModel.java
@@ -1,4 +1,28 @@
 package com.example.asiochatfrontend.ui.profile;
 
-public class ProfileViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.example.asiochatfrontend.core.connection.ConnectionManager;
+import com.example.asiochatfrontend.core.model.dto.UserDto;
+import com.example.asiochatfrontend.domain.usecase.user.GetUserByIdUseCase;
+
+import javax.inject.Inject;
+
+public class ProfileViewModel extends ViewModel {
+    private final MutableLiveData<UserDto> profile = new MutableLiveData<>();
+
+    @Inject
+    public ProfileViewModel(ConnectionManager connectionManager) {
+        try {
+            GetUserByIdUseCase useCase = new GetUserByIdUseCase(connectionManager);
+            profile.setValue(useCase.execute("current_user_id"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public LiveData<UserDto> getProfile() {
+        return profile;
+    }
 }
